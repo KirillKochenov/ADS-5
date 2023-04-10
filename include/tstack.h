e9e2cbdd0fc721ec6c7d411b6910e7b413967120
KirillKochenov <124993480+KirillKochenov@users.noreply.github.com>
2023-04-10 22:30:14 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,34 +2,9 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size = 0>
+template<typename T, int size>
 class TStack {
- private:
-    T arr[size] = {0};
-    int top;
-
- public:
-    TStack() {
-        top = -1;
-    }
-    void Push(const T& value) {
-        if (!isFull()) {
-            arr[++top] = value;
-        }
-    }
-    T get() const {
-        return arr[top];
-    }
-    T pop() {
-        return arr[top--];
-    }
-    int GetTop() const { return top; }
-    bool isEmpty() const {
-        return top == -1;
-    }
-    bool isFull() const {
-        return top == size;
-    }
+  // добавьте код стека
 };
 
 #endif  // INCLUDE_TSTACK_H_
